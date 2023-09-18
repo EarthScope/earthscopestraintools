1cda8c3906632e2235eec7a0aee80b8de758840f
cehanagan <74363782+cehanagan@users.noreply.github.com>
2023-09-18 10:17:38 -0700

    Edit conf.py

--- docs/build/html/_sources/index.rst.txt
+++ docs/build/html/_sources/index.rst.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Earthscope Strain Tools's documentation!
+Welcome to Earthscope Strain Tools documentation!
 ===================================================
 
 .. toctree::
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +28,10 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+#html_logo = "images/cig_short_pylith.png"
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+#import pathlib
+#import sys
+#sys.path.insert(0, pathlib.Path('src/earthscopestraintools/timeseries.py').parents[2].resolve().as_posix())
